d8c1bbd41cee4cab5dc013d5eba9c444b1b80403
Valeria <70991795+VaLeRy013@users.noreply.github.com>
2020-11-11 12:20:49 +0300

    Add files via upload

---
+++ problema nr 1.py
@@ -0,0 +1,13 @@
+n=int(input("dati valoarea lui n:"))
+if n<28:
+    print("luna cu",n,"de zile nu exista")
+if n==28:
+    print("luna cu",n,"de zile este februaie")
+if n==29:
+    print("luna cu",n,"de zile este februaie")
+if n==30:
+    print("lunile cu",n,"de zile sunt aprilie,iunie,septembrie,noiembrie")
+if n==31:
+    print("lunile cu",n,"de zile sunt ianuarie, mai, iulie, august, octombrie, decembrie")
+if n>31:
+    print("luna cu",n,"de zile nu exista")
